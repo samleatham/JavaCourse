64bcb61fd05571da7f0d83ef3e571d90c3a1a020
sam.leatham97@gmail.com <sam.leatham97@gmail.com>
2021-06-05 14:21:39 +0100

    adding a static console class

--- src/com/sam/Main.java
+++ src/com/sam/Main.java
@@ -1,33 +1,19 @@
 package com.sam;
 
-import java.text.NumberFormat;
-import java.util.Scanner;
+import som.sam.Console;
 
 public class Main {
 
 
     public static void main(String[] args) {
-        int principal = (int) readNumber("Principal: ", 1000, 1_000_000);
-        float annualInterest = (float) readNumber("Annual Interest Rate: ", 1, 30);
-        byte years = (byte) readNumber("Period (Years): ", 1, 30);
+        int principal = (int) Console.readNumber("Principal: ", 1000, 1_000_000);
+        float annualInterest = (float) Console.readNumber("Annual Interest Rate: ", 1, 30);
+        byte years = (byte) Console.readNumber("Period (Years): ", 1, 30);
 
         var mortgage = new Mortgage(principal,annualInterest,years);
 
         mortgage.printMortgage();
         mortgage.printPaymentSchedule();
-    }
-
-    public static double readNumber(String prompt, double min, double max) {
-        Scanner scanner = new Scanner(System.in);
-        double value;
-        while (true) {
-            System.out.print(prompt);
-            value = scanner.nextFloat();
-            if (value >= min && value <= max)
-                break;
-            System.out.println("Enter a value between " + min + " and " + max);
-        }
-        return value;
     }
 
 }
